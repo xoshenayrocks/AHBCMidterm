2a38ae7108477fbe06338c4d7e4eba8e3faee90a
Alejandra Roel <alejandraroel@Alejandras-MacBook-Air-2.local>
2020-02-27 18:42:09 -0500

    Subtotals

---
+++ POSMidterm/SubtotalBill.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace midterm
+
+{
+    public static class SubtotalBill
+    {
+
+        public static double GetTotal(double cost, double qty)
+        {
+            double total = cost * qty;
+            return total;
+        }
+
+        public static double GetActualTotal(double cost, double qty)
+        {
+            double actualTotal = GetTotal(cost, qty) * 1.06;
+            return actualTotal;
+        }
+
+        public static double GetChange(double amountTendered, double actualTotal)
+        {
+            double changeTotal = amountTendered - actualTotal;
+            return changeTotal;
+        }
+
+
+
+
+        //below, what i think is happening is that there is a
+
+        
+    }
+
+}
+
+   
