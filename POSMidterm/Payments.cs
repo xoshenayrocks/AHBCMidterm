6d4abf44e9f47f9fdeae9abdda98a93659f23dfa
xoshenayrocks <shenay.russell@gmail.com>
2020-02-27 20:21:51 -0500

    updating local

---
+++ POSMidterm/Payments.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace POSMidterm
+{
+    public class Payments
+    {
+        public Payments()
+        {
+        }
+
+        public static void GetPaymentType ()
+        {
+            Console.WriteLine("How would you like to pay for your items? [1] Cash, [2] Credit, or [3] Check?");
+            string userInput = Console.ReadLine();
+            if (userInput == "1" ||userInput.Equals("cash", StringComparison.OrdinalIgnoreCase))
+            {
+
+                Console.WriteLine($"Change due: ${SubtotalBill.GetChange(GetCashPayment(), SubtotalBill.GetActualTotal(1, 2))} "); 
+                Console.WriteLine("Thank you! Enjoy!");
+                //need to add get change method from Alejandra
+            }
+            else if (userInput == "2" || userInput.Equals("credit", StringComparison.OrdinalIgnoreCase))
+            {
+                ValidateCreditCardInfo();
+            }
+            else if (userInput == "3" || userInput.Equals("check", StringComparison.OrdinalIgnoreCase))
+            {
+
+            }
+
+        }
+
+        public static double GetCashPayment()
+        {
+            Console.WriteLine("Please enter cash amount tendered: ");
+            string userInput = Console.ReadLine();
+            double amountTendered = double.Parse(userInput);
+            return amountTendered;
+        }
+
+        public static void GetCheckNumber()
+        {
+            Regex checkNumber = new Regex(@"^([0-9]){4}$");
+            Console.WriteLine("Please enter the check number: " );
+            string userCheckNumber = Console.ReadLine();
+            if (!checkNumber.IsMatch(userCheckNumber))
+            {
+
+            }
+        }
+
+        public static void ValidateCreditCardInfo()
+        {
+
+            Regex creditCard = new Regex(@"(([0-9]){4}){4}");
+            Regex expiration = new Regex(@"^(0[1-9]|1[0-2])\/([0-9]){2}");
+            Regex cvv = new Regex(@"^([0-9]){3}$");
+
+            bool isValid = false;
+            do
+            {
+                Console.WriteLine("Please enter your credit card number, expiration date [MM/YY], and 3-digit CVV number: ");
+          
+                string userInput = Console.ReadLine();
+                string[] creditCardInfo = userInput.Split(" ");
+
+                if (!creditCard.IsMatch(creditCardInfo[0]))
+                {
+                    Console.WriteLine("Card number is not valid. Please enter a valid 16-digit card number.");
+                    creditCardInfo[0] = Console.ReadLine();
+                    isValid = false;
+                }
+                if (!expiration.IsMatch(creditCardInfo[1]))
+                {
+                    Console.WriteLine("Expiration date is not valid. Please enter a valid 2-digit month and 2-digit year.");
+                    creditCardInfo[1] = Console.ReadLine();
+                    isValid = false;
+                }
+
+                if (!cvv.IsMatch(creditCardInfo[2]))
+                {
+                    Console.WriteLine("CVV is not valid. Please enter the 3-digit CVV number on the back of your card.");
+                    creditCardInfo[2] = Console.ReadLine();
+                    isValid = false;
+                }
+                else if (creditCard.IsMatch(creditCardInfo[0]) || expiration.IsMatch(creditCardInfo[1]) || cvv.IsMatch(creditCardInfo[2]))
+                {
+                    Console.WriteLine("Thank you!");
+
+                }
+            } while (isValid);
+
+
+        }
+
+    }
+}
---
+++ POSMidterm/Product.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace POSMidterm
+{
+    public class Product
+
+    {
+        public Product(string foodName, double price)
+        {
+            FoodName = foodName;
+            Price = price;
+        }
+
+        public string FoodName { get; set; }
+        public double Price { get; set; }
+
+       
+    }
+}
